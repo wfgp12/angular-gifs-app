ee75a8f3abc05cacec1a98148f2bb35d90bde1eb
wfgp12 <w.gutierrez1206@gmail.com>
2025-04-07 23:10:28 -0500

    refactor: created DashboardPageComponent and set up routing
    - Created the DashboardPageComponent to hold the main dashboard view.
    - Refactored initial layout code into the new component.
    - Configured main routes and child routes such as 	rending and search.

---
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'gifs-dashboard-page',
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './dashboard-page.component.html',
+})
+export default class DashboardPageComponent { }
