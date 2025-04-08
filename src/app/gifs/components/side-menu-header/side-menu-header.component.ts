cd67cda33063253e6641b142da582ecc484267a0
wfgp12 <w.gutierrez1206@gmail.com>
2025-04-08 00:05:04 -0500

    chore: initial environment configuration setup
    - Added initial structure to manage application environments.
    - Enables handling of environment-specific variables (development, production, etc.).

--- src/app/gifs/components/side-menu-header/side-menu-header.component.ts
+++ src/app/gifs/components/side-menu-header/side-menu-header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'gifs-side-menu-header',
   imports: [],
   templateUrl: './side-menu-header.component.html',
 })
-export class SideMenuHeaderComponent { }
+export class SideMenuHeaderComponent { 
+  envs = environment;
+}
