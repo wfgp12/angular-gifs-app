a5bba7da6b32831f2100cc366c7bdf6d98d5ca2b
wfgp12 <w.gutierrez1206@gmail.com>
2025-04-25 16:10:47 -0500

    feat: implemented masonry-style layout for trending page
    - Applied a masonry layout to display GIFs on the TrendingPage.
    - This enhances the visual presentation and organizes GIFs of varying dimensions more effectively.

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -14,6 +14,14 @@ export class GifsService {
     trendingGifs = signal<Gif[]>([]);
     trendingGifsLoading = signal(true);
 
+    trendingGifGroup = computed<Gif[][]> (() => {
+        const groups = [];
+        for(let i = 0; i < this.trendingGifs().length ; i+=3) {
+            groups.push(this.trendingGifs().slice(i, i+3));
+        }
+        return groups
+    })
+
     searchHistory = signal<Record<string, Gif[]>>(JSON.parse(localStorage.getItem('gifHistory')?? '{}') );
     searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
 
